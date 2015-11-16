eb144ad76bfdcc3c813b7edfcdd921f737e491c2
13Mzawa2 <nishizawa-masahiro-ys@ynu.jp>
2015-11-17 08:54:46 +0900

    ProCamの自動推定とチェスボードの追跡を追加　まだまだ精度に難あり

--- ProCamAutoCalibration/ProCamAutoCalibration.cpp
+++ ProCamAutoCalibration/ProCamAutoCalibration.cpp
@@ -22,9 +22,16 @@ const char* cameraWindowName = "Camera";			//	カメラ画面のID
 //	Results
 Mat cameraMatrix;		//	カメラ内部行列
 Mat distCoeffs;			//	レンズ歪みベクトル
-vector<Mat> rvecs;		//	個々のチェスボードから見たカメラの回転ベクトル
-vector<Mat> tvecs;		//	個々のチェスボードから見たカメラの並進ベクトル
-Mat map1, map2;			//	歪み補正マップ
+Mat cameraMatrixProj;	//	プロジェクタ内部行列
+Mat distCoeffsProj;		//	プロジェクタのレンズ歪みベクトル
+vector<Mat> rvecs, rvecsProj;		//	個々のチェスボードから見たカメラの回転ベクトル
+vector<Mat> tvecs, tvecsProj;		//	個々のチェスボードから見たカメラの並進ベクトル
+Mat map1, map2;				//	歪み補正マップ
+Mat map1Proj, map2Proj;		//	プロジェクタの歪み補正マップ
+Mat RProCam, TProCam;	//	カメラ-プロジェクタ間の回転行列・並進ベクトル
+Mat EProCam, FProCam;	//	カメラ-プロジェクタ間の基本行列，基礎行列
+
+Point3d operator*(Mat M, const Point3d& p);
 
 int main(void)
 {
@@ -146,11 +153,10 @@ int main(void)
 	cout << "Correspondence maps are generated." << endl;
 	gcp.showMaps();
 
-	waitKey(0);
-
 	//-----------------------------------------
 	//	3. プロジェクタキャリブレーション
 	//-----------------------------------------
+	cout << "Calculating Chessboard Corners from Projector..." << endl;
 	//	取得済みのカメラ座標2次元コーナーの歪みを除去
 	vector<vector<Point2f>> corners2dUndistorted;
 	for (int i = 0; i < imgNum; i++)
@@ -189,11 +195,46 @@ int main(void)
 		}
 		corners2dProj.push_back(points);
 	}
-		//	コーナー点を投影(テスト用)
-		Mat mapped(projSize, CV_8UC3, Scalar::all(255));
-		drawChessboardCorners(mapped, patternSize, Mat(corners2dProj[imgNum-1]), true);
-		imshow(projectorWindowName, mapped);
-		waitKey(0);
+	//	コーナー点を投影(テスト用)
+	cout << "Please check whether this chess corners are correct." << endl;
+	Mat mapped(projSize, CV_8UC3, Scalar::all(255));
+	drawChessboardCorners(mapped, patternSize, Mat(corners2dProj[imgNum-1]), true);
+	imshow(projectorWindowName, mapped);
+	waitKey(0);
+
+	//	プロジェクタキャリブレーション
+	//	パラメータ推定
+	cout << "Calculating Projector Inner Matrix and Distortion Parameters..." << endl;
+	cv::calibrateCamera(
+		corners3d, corners2dProj,
+		img.size(),
+		cameraMatrixProj,distCoeffsProj,
+		rvecsProj, tvecsProj);
+	cout << "Projector Camera Matirx = \n" << cameraMatrix << "\nProjector Distortion Coeffs = \n" << distCoeffs << endl;
+	//	歪み補正マップ計算
+	cout << "Making Projector Undistort Map..." << endl;
+	initUndistortRectifyMap(
+		cameraMatrixProj, distCoeffsProj,
+		Mat(), cameraMatrixProj, img.size(), CV_32FC1,
+		map1Proj, map2Proj);
+	//	カメラ - プロジェクタ外部行列の推定
+	cout << "Caluculating Camera-Projector outer parameters..." << endl;
+	cv::stereoCalibrate(
+		corners3d, corners2d, corners2dProj,		//	コーナー点群
+		cameraMatrix, distCoeffs,					//	カメラパラメータ
+		cameraMatrixProj, distCoeffsProj,			//	プロジェクタパラメータ	
+		img.size(),									//	画像サイズ
+		RProCam, TProCam, EProCam, FProCam,			//	カメラに対するプロジェクタの外部パラメータ
+		CALIB_FIX_INTRINSIC,						//	個々に求めた内部パラメータで固定（外部パラメータだけ推定）
+		TermCriteria(TermCriteria::COUNT + TermCriteria::EPS, 30, 1e-6)
+		);
+	cout << "R = \n" << RProCam
+		<< "\nT = \n" << TProCam
+		<< "\nE = \n" << EProCam
+		<< "\nF = \n" << FProCam
+		<< "\nProjector Calibration Ended." << endl;
+	cv::waitKey(0);
+
 
 	// capture loop
 	char key = 0;
@@ -203,17 +244,62 @@ int main(void)
 		flip(image, image, 1);
 		Mat imgUndistorted;
 		remap(image, imgUndistorted, map1, map2, INTER_CUBIC);
+		//	チェスボード検出
+		vector<Point2d> corners;
+		bool patternfound = findChessboardCorners(imgUndistorted, patternSize, corners,
+			CALIB_CB_ADAPTIVE_THRESH | CALIB_CB_NORMALIZE_IMAGE | CALIB_CB_FAST_CHECK);
+		if (patternfound)
+		{	//	見つかった場合
+			Mat rvec1, tvec1, rvec2, tvec2, rvec12, tvec12;
+			solvePnP(corners3d[0], corners, cameraMatrix, Mat(), rvec1, tvec1);	//	回転ベクトル，並進ベクトルを計算
+			Rodrigues(RProCam, rvec12); tvec12 = TProCam.clone();			//	ProCam
+			composeRT(rvec1, tvec1, rvec12, tvec12, rvec2, tvec2);			//	RTp = RTpc * RTc
+			vector<Point2f> cornersProj;
+			for (int i = 0; i < patternSize.width * patternSize.height; i++)
+			{
+				projectPoints(corners3d[0], rvec2, tvec2, cameraMatrixProj, distCoeffsProj, cornersProj);
+			}
+			//	チェスコーナーを描画 上手くいけば重なるはず
+			Mat drawPattern(projSize, CV_8UC3, Scalar::all(255));
+			drawChessboardCorners(drawPattern, patternSize, Mat(cornersProj), patternfound);
+			imshow(projectorWindowName, drawPattern);
+		}
 		cv::imshow("calibrated", imgUndistorted);
 		key = cv::waitKey(1);
 	}
 
 	//	save as XML, PNG
 	FileStorage fs("calibdata.xml", FileStorage::WRITE);
-	fs << "calibration"
+	fs << "Camera"
 		<< "{"
 		<< "CameraMatrix" << cameraMatrix
 		<< "DistCoeffs" << distCoeffs
+		<< "}"
+		<< "Projector"
+		<< "{"
+		<< "CameraMatrix" << cameraMatrixProj
+		<< "Distcoeffs" << distCoeffsProj
+		<< "}"
+		<< "ProCam"
+		<< "{"
+		<< "R" << RProCam
+		<< "T" << TProCam
+		<< "E" << EProCam
+		<< "F" << FProCam
 		<< "}";
 
 	return 0;
+}
+
+//	Point3f = Mat * Point3f　の行列演算オペレータ
+Point3d operator*(Mat M, const Point3d& p)
+{
+	Mat src(3/*rows*/, 1 /* cols */, CV_64F);
+
+	src.at<double>(0, 0) = p.x;
+	src.at<double>(1, 0) = p.y;
+	src.at<double>(2, 0) = p.z;
+
+	Mat dst = M*src; //USE MATRIX ALGEBRA
+	return Point3d(dst.at<double>(0, 0), dst.at<double>(1, 0), dst.at<double>(2, 0));
 }
